0a044a0faad085ecc9fd9a591129d88f698bd938
eLafo <javierlafora@gmail.com>
2016-01-19 20:03:01 +0100

    polymorphic comments

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,10 +1,18 @@
 class Beer < ActiveRecord::Base
 
   belongs_to :user, inverse_of: :beers
-  has_many :comments, class_name: 'BeerComment', inverse_of: :beer
+  has_many :comments, as: :commentable, inverse_of: :commentable
   accepts_nested_attributes_for :comments
 
   validates_presence_of :name
   acts_as_voteable
+
+  # Because rails_admin does not work well with polymorphic associations  https://github.com/sferik/rails_admin/issues/1338
+  # Possible monkeypatch https://github.com/playtestcloud/rails_admin/commit/abf611152e6af6b7394d3c561f24c63d958e194e#comments
+  rails_admin do
+    configure :comments do
+      hide
+    end
+  end
 
 end
--- app/models/beer_comment.rb
+++
@@ -1,4 +0,0 @@
-class BeerComment < ActiveRecord::Base
-  belongs_to :beer, inverse_of: :comments
-  belongs_to :user, inverse_of: :beer_comments
-end
